df002d472b858c441700600facdea4154a26f77f
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2016-03-29 01:30:20 +0200

    Rebuild

--- observe_sequence.js
+++ observe_sequence.js
@@ -11,81 +11,8 @@ module.exports = function(Meteor) {
   var MongoID = Meteor.MongoID;
   var ObserveSequence;
 
-<<<<<<< HEAD
-  //Copied from minimongo package to remove dependency, as this code should go into a helper package
-  var LocalCollection = {};
-  var Package = {minimongo: {LocalCollection: LocalCollection}};
-
-
-// NB: used by livedata
-LocalCollection._idStringify = function (id) {
-  if (id instanceof LocalCollection._ObjectID) {
-    return id.valueOf();
-  } else if (typeof id === 'string') {
-    if (id === "") {
-      return id;
-    } else if (id.substr(0, 1) === "-" || // escape previously dashed strings
-               id.substr(0, 1) === "~" || // escape escaped numbers, true, false
-               LocalCollection._looksLikeObjectID(id) || // escape object-id-form strings
-               id.substr(0, 1) === '{') { // escape object-form strings, for maybe implementing later
-      return "-" + id;
-    } else {
-      return id; // other strings go through unchanged.
-    }
-  } else if (id === undefined) {
-    return '-';
-  } else if (typeof id === 'object' && id !== null) {
-    throw new Error("Meteor does not currently support objects other than ObjectID as ids");
-  } else { // Numbers, true, false, null
-    return "~" + JSON.stringify(id);
-  }
-};
-
-
-// NB: used by livedata
-LocalCollection._idParse = function (id) {
-  if (id === "") {
-    return id;
-  } else if (id === '-') {
-    return undefined;
-  } else if (id.substr(0, 1) === '-') {
-    return id.substr(1);
-  } else if (id.substr(0, 1) === '~') {
-    return JSON.parse(id.substr(1));
-  } else if (LocalCollection._looksLikeObjectID(id)) {
-    return new LocalCollection._ObjectID(id);
-  } else {
-    return id;
-  }
-};
-
-LocalCollection._makeChangedFields = function (newDoc, oldDoc) {
-  var fields = {};
-  LocalCollection._diffObjects(oldDoc, newDoc, {
-    leftOnly: function (key, value) {
-      fields[key] = undefined;
-    },
-    rightOnly: function (key, value) {
-      fields[key] = value;
-    },
-    both: function (key, leftValue, rightValue) {
-      if (!EJSON.equals(leftValue, rightValue))
-        fields[key] = rightValue;
-    }
-  });
-  return fields;
-};
-
-// Is this selector just shorthand for lookup by _id?
-LocalCollection._selectorIsId = function (selector) {
-  return (typeof selector === "string") ||
-    (typeof selector === "number") ||
-    selector instanceof LocalCollection._ObjectID;
-};
-=======
   var Package = {minimongo: {LocalCollection: LocalCollection}, "diff-sequence" : {DiffSequence: DiffSequence}};
 
->>>>>>> meteor-update
 // ordered: bool.
 // old_results and new_results: collections of documents.
 //    if ordered, they are arrays.
